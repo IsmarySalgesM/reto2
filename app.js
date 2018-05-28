e5f2ade2ec98db5ceb2242bc453f54366755b5a2
Ismary Salges <ismarysalges@gmail.com>
2018-05-28 17:59:08 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+//Ceviche, Tacos al Pastor, Empanada chilena, hamburguesa
+
+var ceviche = false;
+var tacosAlPastor = true; 
+var empanadaChilna = false;
+var hamburguesa = false;
+
+if (ceviche) {
+    alert ( "con mucho limon por favor"); 
+}else if (tacosAlPastor) {
+    alert ("con mucho queso por favor");
+}else if  (empanadaChilna) {
+    alert ("Con mucho pebre");
+}else if (hamburguesa) {
+    alert ("Doble queso y Tocino");
+} else {
+    alert("Me ire gracias >)");
+    
+}
